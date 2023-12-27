5142e45e79c3777aac18c807a7927ed4cdfdea69
Code With Naim <111947358+CodeWithNaim@users.noreply.github.com>
2023-12-27 23:06:51 +0600

    added country not found feature

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,24 +4,43 @@ import { LoadingMesssge } from "../components/Loading/Loading";
 import style from "./countries.module.css";
 import { useContext } from "react";
 import { CountriesContext } from "../context/CountriesContext";
+import errorImg from "../assets/images/404Error.gif";
 const Countries = () => {
   const { isError, isLoading, filterdCountries, handleRemoveCountry } =
     useContext(CountriesContext);
+
   return (
     <>
       {isLoading && <LoadingMesssge messageName="Countries" />}
       {isError && <ErrorMesssge />}
 
-      <div className={style.countries}>
-        {filterdCountries.map((country) => {
-          return (
-            <Country
-              handleRemoveCountry={handleRemoveCountry}
-              key={country.name.common}
-              country={country}
-            />
-          );
-        })}
+      <div>
+        {filterdCountries.length > 0 ? (
+          <div className={style.countries}>
+            {filterdCountries.map((country) => {
+              return (
+                <Country
+                  handleRemoveCountry={handleRemoveCountry}
+                  key={country.name.common}
+                  country={country}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <div
+            style={{
+              textAlign: "center",
+              display: "block",
+            }}
+          >
+            <img style={{ width: "300px" }} src={errorImg} alt="error image" />
+            <h2 style={{ color: "red" }}>
+              {" "}
+              <b>Error:</b> Country Not Found! Enter Valid Name again.
+            </h2>
+          </div>
+        )}
       </div>
     </>
   );
